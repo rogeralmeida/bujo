912ae4e41f255da6caa37bf7a67fcb5aa1a80beb
Roger Almeida <roger.eduardo@gmail.com>
2018-01-09 04:55:53 +1100

    Pomodoro command

--- bujo.py
+++ bujo.py
@@ -1,5 +1,8 @@
 import click
 from tinydb import TinyDB, Query, where
+from tqdm import tqdm
+import time
+import os
 
 TASK_DATABASE='tasks.json'
 
@@ -30,3 +33,14 @@ def list():
   tasks = TinyDB(TASK_DATABASE)
   for task in tasks:
     print("{}    {}  {}".format(task.doc_id, task['kind'], task['text']))
+
+@cli.command()
+@click.argument('task_id')
+def pomodoro(task_id):
+  tasks = TinyDB(TASK_DATABASE)
+  task = tasks.get(doc_id=int(task_id))
+  click.echo("Starting pomodoro on task: {}".format(task['text']))
+  for i in tqdm(range(1500)):
+    time.sleep(0.1)
+  os.system('terminal-notifier -title "Pomodoro Done" -message "'+task['text'] +'" -sound default')
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bujo',
     version='0.0.1',
     py_modules=['bujo'],
-    install_requires=['Click', 'TinyDB'],
+    install_requires=['Click', 'TinyDB', 'tqdm'],
     entry_points='''
         [console_scripts]
         bujo=bujo:cli
