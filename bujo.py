48eb2a7c165b398321fb715ee75bf18036b76da5
Roger Almeida <roger.eduardo@gmail.com>
2018-01-09 04:21:28 +1100

    Refactoring: extracted task database name to constant

--- bujo.py
+++ bujo.py
@@ -1,6 +1,8 @@
 import click
 from tinydb import TinyDB, Query, where
 
+TASK_DATABASE='tasks.json'
+
 @click.group()
 def cli():
   pass
@@ -10,7 +12,7 @@ def cli():
 @click.argument('text', nargs=-1)
 def add(kind, text):
   """ Add a new task to the task list"""
-  tasks = TinyDB('tasks.json')
+  tasks = TinyDB(TASK_DATABASE)
   tasks.insert({'kind': kind, 'text': " ".join(text)})
   for task in tasks:
     print("{}    {}  {}".format(task.doc_id, task['kind'], task['text']))
@@ -19,12 +21,12 @@ def add(kind, text):
 @click.argument('task_id')
 def delete(task_id):
   """ Remove a task from the task list """
-  tasks = TinyDB('tasks.json')
+  tasks = TinyDB(TASK_DATABASE)
   tasks.remove(doc_ids=[int(task_id)])
 
 @cli.command()
 def list():
   """ List current tasks """
-  tasks = TinyDB('tasks.json')
+  tasks = TinyDB(TASK_DATABASE)
   for task in tasks:
     print("{}    {}  {}".format(task.doc_id, task['kind'], task['text']))
